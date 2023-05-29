855e5426bc33d63c1dfc9ac2c08c07d3bd811328
Sarah Quinney <quinney.sarah@gmail.com>
2023-05-28 23:12:09 -0500

    Fixed color change buttons.

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+let color = "black"
+
 //function to make the size of the grid changable.//
 function changeGrid(size) {
 
@@ -20,6 +22,9 @@ function changeGrid(size) {
     for (let i = 0; i < amount; i++) {
         //creates grid squares//
         let gridSquare = document.createElement("div");
+        //Event listener click to make squares change color when clicked//
+        gridSquare.addEventListener("click", squareColor)
+
         //colors grid squares pink for visual//
         gridSquare.style.backgroundColor = "pink";
         //inserts gridSquare into grid id (from html)//
@@ -37,4 +42,21 @@ function changeSize(input) {
     else {
         alert("Invalid input! Choose a number between 2 and 100.");
     }
+}
+
+//this function calls a function each square to make it shuffle color or black (black = color) global let//
+function squareColor() {
+    console.log(this);
+    if (color === "shuffle") {
+        this.style.backgroundColor = `hsl(${Math.random() * 360}, 100%, 50%)`;
+    }
+
+    else {
+        this.style.backgroundColor = color;
+    }
+}
+
+//lets user choose color based on button selection clicked//
+function colorChange(choose) {
+    color = choose;
 }
